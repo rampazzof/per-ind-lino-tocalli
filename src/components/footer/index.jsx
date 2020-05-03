54b7b6ea193a4f5b9ee5fe670db6ea3157430809
Federico Rampazzo <federico.rampazzo@mondora.com>
2020-05-03 17:33:45 +0200

    chore: remove chakra items init

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,35 +1,23 @@
 import React from "react"
 
-import { Box, Grid, Image } from "@chakra-ui/core"
-
 import logoBlack from "../../images/logo-black.jpg"
 
+import "./style.css"
+
 const Footer = () => (
-  <Grid
-    templateColumns="repeat(2, 1fr)"
-    columnGap={6}
-    bg="black"
-    color="white"
-    fontSize="lg"
-    fontWeight="bold"
-    pt={4}
-    pl={8}
-    pr={8}
-  >
-    <Box id="contacts" ml={4}>
-      <Box>Per. Ind. Lino Tocalli</Box>
-      <Box>Via Serta, 190 - 23017 Morbegno (SO)</Box>
-      <Box mt={2}>Tel.: +39.349.28.10.172</Box>
-      <Box>mail: lino.tocalli@live.com</Box>
-      <Box mt={2} mb={4}>
-        Pec: lino.tocalli@pec.eppi.it
-      </Box>
-    </Box>
-    <Box mr={4} mb={4} textAlign="right">
-      <Image className="svg-up" src={logoBlack} h="118px" ml="auto" mr="0" />
-      P. Iva 00948340146
-    </Box>
-  </Grid>
+  <div className="footer">
+    <div id="contacts" className="footer-item">
+      <div>Per. Ind. Lino Tocalli</div>
+      <div>Via Serta, 190 - 23017 Morbegno (SO)</div>
+      <div mt={2}>Tel.: +39.349.28.10.172</div>
+      <div>mail: lino.tocalli@live.com</div>
+      <div>Pec: lino.tocalli@pec.eppi.it</div>
+    </div>
+    <div className="footer-item">
+      <img className="footer-img" src={logoBlack} />
+      <div className="right">P. Iva 00948340146</div>
+    </div>
+  </div>
 )
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,8 @@
 import React from "react"
 import "typeface-chakra-petch"
 
-import { Link as GatsbyLink } from "gatsby"
-import {
-  Box,
-  Button,
-  Grid,
-  Image,
-  Link,
-  List,
-  ListItem,
-  ListIcon,
-  Text,
-} from "@chakra-ui/core"
+import { Link } from "gatsby"
+import { Button } from "@chakra-ui/core"
 
 import logo from "../images/logo.jpg"
 import LeggiJpg from "../images/OBBLIGHI.jpg"
@@ -22,18 +12,16 @@ import DomoticaJpg from "../images/DOMOTICA.jpg"
 import ServiziJpg from "../images/SERVIZI.jpg"
 
 export default () => (
-  <Box>
-    <Grid
-      templateColumns="repeat(2, 1fr)"
-      alignItems="center"
-      h="calc(100vh - 90px)"
-    >
-      <Image className="svg-up" src={logo} h="450px" ml="auto" mr="auto" />
-      <Box fontSize="6xl" fontWeight="bold" pl={24}>
-        <Text>
+  <div>
+    <div className="main-presentation">
+      <div className="main-item">
+        <img className="main-logo" src={logo} alt="main logo" />
+      </div>
+      <div className="main-item">
+        <div className="main-item-title">
           Progettazione e consulenza impianti elettrici, speciali e fotovoltaici
-        </Text>
-        <Link as={GatsbyLink} to="/presentazione" float="left">
+        </div>
+        <Link to="/presentazione" float="left">
           <Button
             color="#bfff00"
             bg="black"
@@ -49,280 +37,166 @@ export default () => (
             Scopri di più
           </Button>
         </Link>
-      </Box>
-    </Grid>
+      </div>
+    </div>
     <div className="gradient-bar" />
-    <Box color="white">
-      <Grid
-        h="50vh"
-        templateColumns="repeat(3, 1fr)"
-        columnGap={6}
-        bg="#192e26ff"
-        alignItems="center"
-      >
-        <Box fontWeight="bold" fontSize="45xl" pl={8} pr={8}>
-          PROGETTAZIONE IMPIANTI ELETTRICI
-          <Box fontWeight="normal" fontSize="md">
-            Nell'ambito civile, industriale e terziario, lo studio ha le
-            competenze per la progettazione e la direzione lavori di impianti
-            elettrici a 360 gradi.
-          </Box>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Box fontWeight="bold">
-            Progettazione impianti elettrici in media e bassa tensione:
-          </Box>
-          <List>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              quadri elettrici;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              sistemi di distribuzione dell'energia;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti forza motrice;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di illuminazione (ordinaria e di emergenza);
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di messa a terra ed equipotenziali.
-            </ListItem>
-          </List>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Image
-            src={ElettriciJpg}
-            alt="Impianti elettrici jpg"
-            opacity="0.3"
-            h="275px"
-            w="475px"
-          />
-        </Box>
-      </Grid>
-      <Grid
-        h="50vh"
-        templateColumns="repeat(3, 1fr)"
-        gap={6}
-        bg="#214e34ff"
-        alignItems="center"
-      >
-        <Box pl={8} pr={8}>
-          <Image
-            src={DomoticaJpg}
-            alt="Impianti speciali jpg"
-            opacity="0.3"
-            h="275px"
-            w="475px"
-          />
-        </Box>
-        <Box fontWeight="bold" fontSize="45xl" pl={8} pr={8}>
-          PROGETTAZIONE IMPIANTI SPECIALI
-          <Box fontWeight="normal" fontSize="md">
-            Progettazione e direzione lavori di impianti a correnti deboli e di
-            segnale, denominati impianti speciali.
-          </Box>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Box fontWeight="bold">
-            Progettazione impianti speciali in vari settori:
-          </Box>
-          <List>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              domotica;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti antintrusione;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di rivelazione ed allarme incendi;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di evacuazione EVAC;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di videosorveglianza;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di ricezione segnali TV/SAT;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di cablaggio strutturato;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              impianti di diffusione sonora audio.
-            </ListItem>
-          </List>
-        </Box>
-      </Grid>
-      <Grid
-        h="50vh"
-        templateColumns="repeat(3, 1fr)"
-        gap={6}
-        bg="#5c7457ff"
-        alignItems="center"
-      >
-        <Box fontWeight="bold" fontSize="45xl" pl={8} pr={8}>
+    <div className="row-container green-5">
+      <div className="row-item">
+        <div className="row-item-title">PROGETTAZIONE IMPIANTI ELETTRICI</div>
+        <div>
+          Nell'ambito civile, industriale e terziario, lo studio ha le
+          competenze per la progettazione e la direzione lavori di impianti
+          elettrici a 360 gradi.
+        </div>
+      </div>
+      <div className="row-item">
+        <div className="row-item-list-title">
+          Progettazione impianti elettrici in media e bassa tensione:
+        </div>
+        <ul>
+          <li>quadri elettrici;</li>
+          <li>sistemi di distribuzione dell'energia;</li>
+          <li>impianti forza motrice;</li>
+          <li>impianti di illuminazione (ordinaria e di emergenza);</li>
+          <li>impianti di messa a terra ed equipotenziali.</li>
+        </ul>
+      </div>
+      <div className="row-item">
+        <img
+          className="row-item-image"
+          src={ElettriciJpg}
+          alt="Impianti elettrici jpg"
+        />
+      </div>
+    </div>
+    <div className="row-container green-4">
+      <div className="row-item">
+        <img
+          className="row-item-image"
+          src={DomoticaJpg}
+          alt="Impianti speciali jpg"
+        />
+      </div>
+      <div className="row-item">
+        <div className="row-item-title">PROGETTAZIONE IMPIANTI SPECIALI</div>
+        <div>
+          Progettazione e direzione lavori di impianti a correnti deboli e di
+          segnale, denominati impianti speciali.
+        </div>
+      </div>
+      <div className="row-item">
+        <div>Progettazione impianti speciali in vari settori:</div>
+        <ul>
+          <li>domotica;</li>
+          <li>impianti antintrusione;</li>
+          <li>impianti di rivelazione ed allarme incendi;</li>
+          <li>impianti di evacuazione EVAC;</li>
+          <li>impianti di videosorveglianza;</li>
+          <li>impianti di ricezione segnali TV/SAT;</li>
+          <li>impianti di cablaggio strutturato;</li>
+          <li>impianti di diffusione sonora audio.</li>
+        </ul>
+      </div>
+    </div>
+    <div className="row-container green-3">
+      <div className="row-item">
+        <div className="row-item-title">
           PROGETTAZIONE IMPIANTI FOTOVOLTAICI
-          <Box fontWeight="normal" fontSize="md">
-            Progettazione e gestione pratiche burocratiche di impianti di
-            produzione da energia solare.
-          </Box>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Box fontWeight="bold">Progettazione impianti fotovoltaici:</Box>
-          <List>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              sopralluoghi sul posto e studi di fattibilità;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              business plan e analisi tempi di ammortamento;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              scelta dei principali componenti (moduli FV e inverter);
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              elaborazione di schemi elettrici e planimetrici per
-              l'Installatore;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              gestione pratiche con i vari Enti coinvolti.
-            </ListItem>
-          </List>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Image
-            src={FotovoltaiciJpg}
-            alt="Impianti fotovoltaici jpg"
-            opacity="0.3"
-            h="275px"
-            w="475px"
-          />
-        </Box>
-      </Grid>
-      <Grid
-        h="50vh"
-        templateColumns="repeat(3, 1fr)"
-        gap={6}
-        bg="#475A49"
-        alignItems="center"
-      >
-        <Box pl={8} pr={8}>
-          <Image
-            src={ServiziJpg}
-            alt="Servizi jpg"
-            opacity="0.3"
-            h="275px"
-            w="475px"
-          />
-        </Box>
-        <Box fontWeight="bold" fontSize="45xl" pl={8} pr={8}>
-          SERVIZI
-          <Box fontWeight="normal" fontSize="md">
-            Nei settori sopra citati, lo studio offre servizi di consulenza
-            tecnica, analisi dati, verifica di impianti, collaudi e messe in
-            servizio.
-          </Box>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Box fontWeight="bold">Servizi:</Box>
-          <List>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              consulenza tecnica generale;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              analisi del rischio da scariche atmosferiche;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              dimensionamenti e calcoli elettrotecnici;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              collaudi e messe in servizio;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              calcoli, verifiche e rendering illuminotecnici;
-            </ListItem>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              verifiche periodiche impianti elettrici Guida CEI 64-14.
-            </ListItem>
-          </List>
-        </Box>
-      </Grid>
-      <Grid
-        h="50vh"
-        templateColumns="repeat(3, 1fr)"
-        gap={6}
-        bg="#979B8D"
-        alignItems="center"
-      >
-        <Box fontWeight="bold" fontSize="45xl" pl={8} pr={8}>
+        </div>
+        <div>
+          Progettazione e gestione pratiche burocratiche di impianti di
+          produzione da energia solare.
+        </div>
+      </div>
+      <div className="row-item">
+        <div>Progettazione impianti fotovoltaici:</div>
+        <ul>
+          <li>sopralluoghi sul posto e studi di fattibilità;</li>
+          <li>business plan e analisi tempi di ammortamento;</li>
+          <li>scelta dei principali componenti (moduli FV e inverter);</li>
+          <li>
+            elaborazione di schemi elettrici e planimetrici per l'Installatore;
+          </li>
+          <li>gestione pratiche con i vari Enti coinvolti.</li>
+        </ul>
+      </div>
+      <div className="row-item">
+        <img
+          className="row-item-image"
+          src={FotovoltaiciJpg}
+          alt="Impianti fotovoltaici jpg"
+        />
+      </div>
+    </div>
+    <div className="row-container green-2">
+      <div className="row-item">
+        <img className="row-item-image" src={ServiziJpg} alt="Servizi jpg" />
+      </div>
+      <div className="row-item">
+        <div className="row-item-title">SERVIZI</div>
+        <div>
+          Nei settori sopra citati, lo studio offre servizi di consulenza
+          tecnica, analisi dati, verifica di impianti, collaudi e messe in
+          servizio.
+        </div>
+      </div>
+      <div className="row-item">
+        <div className="row-item-list-title">Servizi:</div>
+        <ul>
+          <li>consulenza tecnica generale;</li>
+          <li>analisi del rischio da scariche atmosferiche;</li>
+          <li>dimensionamenti e calcoli elettrotecnici;</li>
+          <li>collaudi e messe in servizio;</li>
+          <li>calcoli, verifiche e rendering illuminotecnici;</li>
+          <li>verifiche periodiche impianti elettrici Guida CEI 64-14.</li>
+        </ul>
+      </div>
+    </div>
+    <div className="row-container green-1">
+      <div className="row-item">
+        <div className="row-item-title">
           CENNI NORMATIVI ED OBBLIGHI DI LEGGE
-          <Box fontWeight="normal" fontSize="md">
-            Progettazione impianti in accordo alle prescrizioni riportate nel
-            D.Lgs 81/08 e s.m.i. e D.M. 37/08.
-          </Box>
-          <Link as={GatsbyLink} to="/obblighi-e-norme" float="left">
-            <Button
-              color="#bfff00"
-              bg="black"
-              rightIcon="arrow-forward"
-              fontSize="xl"
-              fontWeight="bold"
-              rounded="20px"
-              _focus={{
-                boxShadow: "",
-              }}
-              _hover={{ bg: "#565656" }}
-            >
-              Scopri di più
-            </Button>
-          </Link>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Box fontWeight="bold">Prescrizioni D.Lgs 81/08 e D.M. 37/08:</Box>
-          <List>
-            <ListItem>
-              <ListIcon icon="chevron-right" />
-              Nozioni fondamentali espresse dai due decreti più importanti in
-              materia di impianti elettrici D.Lgs 81/08 e D.M. 37/08.
-            </ListItem>
-          </List>
-        </Box>
-        <Box pl={8} pr={8}>
-          <Image
-            src={LeggiJpg}
-            alt="Obblighi e norme jpg"
-            opacity="0.3"
-            h="275px"
-            w="475px"
-          />
-        </Box>
-      </Grid>
-    </Box>
+        </div>
+        <div>
+          Progettazione impianti in accordo alle prescrizioni riportate nel
+          D.Lgs 81/08 e s.m.i. e D.M. 37/08.
+        </div>
+        <Link to="/obblighi-e-norme" float="left">
+          <Button
+            color="#bfff00"
+            bg="black"
+            rightIcon="arrow-forward"
+            fontSize="xl"
+            fontWeight="bold"
+            rounded="20px"
+            _focus={{
+              boxShadow: "",
+            }}
+            _hover={{ bg: "#565656" }}
+          >
+            Scopri di più
+          </Button>
+        </Link>
+      </div>
+      <div className="row-item">
+        <div className="row-item-list-title">
+          Prescrizioni D.Lgs 81/08 e D.M. 37/08:
+        </div>
+        <ul>
+          <li>
+            Nozioni fondamentali espresse dai due decreti più importanti in
+            materia di impianti elettrici D.Lgs 81/08 e D.M. 37/08.
+          </li>
+        </ul>
+      </div>
+      <div className="row-item">
+        <img
+          className="row-item-image"
+          src={LeggiJpg}
+          alt="Obblighi e norme jpg"
+        />
+      </div>
+    </div>
     <iframe
       id="googlemaps"
       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11052.910261856712!2d9.710459248657225!3d46.16612556580803!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478473103c6c5d73%3A0xedd4a393d5c39f7a!2sVia%20Serta%2C%20190%2C%2023017%20Morbegno%20SO!5e0!3m2!1sit!2sit!4v1588360807863!5m2!1sit!2sit"
@@ -333,5 +207,5 @@ export default () => (
       allowfullscreen=""
       aria-hidden="false"
     />
-  </Box>
+  </div>
 )
